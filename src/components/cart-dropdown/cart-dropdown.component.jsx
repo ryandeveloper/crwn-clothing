e8dad0e89c3185b6e3613af11f4b186583490d5b
ryand <ryandumajil@gmail.com>
2020-11-07 21:43:44 +0800

    Created cart-item component and connected cart dropdown with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import {connect } from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button.component';
-//import CartItem from '../cart-item/cart-item.component'
+import CartItem from '../cart-item/cart-item.component'
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-   <div className='cart-dropdown'>
-      <div className="cart-items">
-         {/* {cartItems.map(cartItem => (
-            <CartItem key={cartItem.id} item={cartItem} />
-         ))} */}
+const CartDropdown = ({cartItems}) => {
+   return (
+      <div className='cart-dropdown'>
+         <div className="cart-items">
+            {cartItems && cartItems.map(cartItem => (
+               <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+         </div>
+         <CustomButton>GO TO CHECKOUT</CustomButton>
       </div>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-   </div>
-)
+   )
+}
 
-const mapStateToProps = ({cart: {cartItems}}) => (
-   cartItems
-)
+const mapStateToProps = (state) => {
+   const { cartItems } = state.cart
+   return {
+      cartItems
+   }
+}
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: {imageUrl, price, name, quantity}}) => {
+const CartItem = ({ item }) => {
+   const {imageUrl='', price='0.00', name='', quantity=''} = item
    return (
       <div className="cart-item">
-         <img src={imageUrl} alt={item}/>
+         <img src={imageUrl} alt={name}/>
          <div className="item-details">
             <span className="name">{name}</span>
             <span className="price">
